03c187326a2d2c4b178d3348d18ef357cf94337a
Chetan Atrawalkar <chetanatrawalkar1@gmail.com>
2021-06-07 17:39:56 +0530

    add netlify folder

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(props) {
+  return (
+    <div className="footer">
+      <p>Ⓒ2020 Programmig with CA:)</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { AiOutlineDelete } from "react-icons/ai";
+
+function Note({ title, content, onDelete, id }) {
+  return (
+    <div className="note">
+      <h1>{title}</h1>
+      <p>{content}</p>
+      <button onClick={() => onDelete(id)}>
+        <AiOutlineDelete size={25} />
+      </button>
+    </div>
+  );
+}
+
+export default Note;
